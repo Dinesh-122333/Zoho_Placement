cb5010a7bddb0049e40d9b70e3ccabe01295de3c
Dinesh-122333 <dineshsrinivasanr77529@gmail.com>
2023-12-01 11:06:25 +0530

    Added file

---
+++ Longest_substring_without_repating_char.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s)>0 :
+            char_index_map = {}
+            start = 0
+            max_length = 0
+
+            for end in range(len(s)):
+                if s[end] in char_index_map and char_index_map[s[end]] >= start:
+                    start = char_index_map[s[end]] + 1
+
+                char_index_map[s[end]] = end
+                max_length = max(max_length, end - start + 1)
+
+            return max_length
+        else:
+            return 0   
