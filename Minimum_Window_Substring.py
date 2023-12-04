48c26ffa1733a86449b16e51d828bc1acdb614b6
DINESH S <119110395+Dinesh-122333@users.noreply.github.com>
2023-12-05 01:03:18 +0530

    Update Minimum_Window_Substring.py

--- Minimum_Window_Substring.py
+++ Minimum_Window_Substring.py
@@ -1,5 +1,49 @@
  def minWindow(self, s: str, t: str) -> str:
-        if len(s) >= len(t):
+     #Brute Force(Test Case Failed) 
+     if len(s) >= len(t):
+          t_dic = {}
+
+          for i in range(len(t)):
+              if t[i] in t_dic:
+                  t_dic[t[i]] += 1
+              else:
+                  t_dic[t[i]] = 1
+
+          counter = 0
+          i, j = 0, 0
+          min_len = float('inf')
+          index_i, index_j = 0, 0
+
+          while j < len(s):
+              if s[j] in t_dic:
+                  if t_dic[s[j]] > 0:
+                      counter += 1
+                  t_dic[s[j]] -= 1
+
+              while counter == len(t):
+                  if j - i < min_len:
+                      min_len = j - i
+                      index_i = i
+                      index_j = j
+
+                  if s[i] in t_dic:
+                      t_dic[s[i]] += 1
+                      if t_dic[s[i]] > 0:
+                          counter -= 1
+
+                  i += 1
+
+              j += 1
+
+          return s[index_i:index_j + 1]
+
+      else:
+          return ""
+      # Time complexity: O(n)
+      # Space complexity: O(len(t))
+  
+      #optimal Way 
+      if len(s) >= len(t):
             t_dic = {}
 
             for i in range(len(t)):
@@ -38,3 +82,5 @@
 
         else:
             return ""
+         # Time complexity: O(n)
+         # Space complexity: O(len(t))
